ea2a229262e2cdfcadc1b3067d837f7bb9a796b6
Nazlı Tunçer <nazlituncer@MacBook-Pro.local>
2021-05-01 21:04:12 +0300

    Hello World Base

--- src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.stacksimplify.restservices.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
 	public String helloWorld() {
 		return "HelloWorld";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Kalyan", "Reddy", "Hyderabad");
 	}
 
 }
